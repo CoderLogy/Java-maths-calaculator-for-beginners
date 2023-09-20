12eb4dfc48491566b03c62c669cc711bad452d61
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2023-09-20 14:23:44 -0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,5 @@
 import java.util.Scanner; 
-class hello
-{
+public class main{
     public static void main (String args[ ]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first no.");
